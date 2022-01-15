fdd00c618d3f57ef0ebf149492c74631dfebc874
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-01-16 00:11:02 +0800

    add product_carousel widget

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -34,7 +34,7 @@ class Product extends Equatable {
     Product(name: 'Soft Drink #1',
         category: 'Soft Drinks',
         imageUrl: 'https://images.unsplash.com/photo-1499638673689-79a0b5115d87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
-        price: 2.99,
+        price: 1.99,
         isPopular: true,
         isRecommended: false),
 
@@ -48,7 +48,7 @@ class Product extends Equatable {
     Product(name: 'Soft Drink #3',
         category: 'Soft Drinks',
         imageUrl: 'https://images.unsplash.com/photo-1527960471264-932f39eb5846?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-        price: 2.99,
+        price: 5.99,
         isPopular: true,
         isRecommended: false)
   ];
