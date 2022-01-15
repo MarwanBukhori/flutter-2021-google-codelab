d776acf7eb50212ebb1f309d47740b51085da6c9
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-01-15 23:53:51 +0800

    add product_card widget

---
+++ lib/models/product_model.dart
@@ -0,0 +1,57 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String name;
+  final String category;
+  final String imageUrl;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
+
+  const Product({
+    required this.name,
+    required this.category,
+    required this.imageUrl,
+    required this.price,
+    required this.isPopular,
+    required this.isRecommended
+  });
+
+
+
+  @override
+  List<Object?> get props =>
+      [
+        name,
+        category,
+        imageUrl,
+        price,
+        isPopular,
+        isRecommended
+      ];
+
+  static List<Product> products = [
+    Product(name: 'Soft Drink #1',
+        category: 'Soft Drinks',
+        imageUrl: 'https://images.unsplash.com/photo-1499638673689-79a0b5115d87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+        price: 2.99,
+        isPopular: true,
+        isRecommended: false),
+
+    Product(name: 'Soft Drink #2',
+        category: 'Soft Drinks',
+        imageUrl: 'https://images.unsplash.com/photo-1527960471264-932f39eb5846?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        price: 2.99,
+        isPopular: true,
+        isRecommended: false),
+
+    Product(name: 'Soft Drink #3',
+        category: 'Soft Drinks',
+        imageUrl: 'https://images.unsplash.com/photo-1527960471264-932f39eb5846?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        price: 2.99,
+        isPopular: true,
+        isRecommended: false)
+  ];
+
+
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -6,15 +6,15 @@ import 'package:flutter_codelab/widgets/widgets.dart';
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
 
-  static Route route(){
+  static Route route() {
     return MaterialPageRoute(
       settings: RouteSettings(name: routeName),
       builder: (_) => HomeScreen(),
     );
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: CustomAppBar(title: 'Kedaiku'),
       bottomNavigationBar: CustomNavBar(),
@@ -22,23 +22,21 @@ class HomeScreen extends StatelessWidget {
         children: [
           Container(
               child: CarouselSlider(
-                options: CarouselOptions(
-                  aspectRatio: 1.5, //make it taller
-                  viewportFraction: 0.9, //make it wider
-                  enlargeCenterPage: true,
-                  enlargeStrategy: CenterPageEnlargeStrategy.height,
-                ),
-                items: Category.categories
-                    .map((category) => HeroCarouselCard(category: category))
-                    .toList(),
-              )
-          ),
+            options: CarouselOptions(
+              aspectRatio: 1.5, //make it taller
+              viewportFraction: 0.9, //make it wider
+              enlargeCenterPage: true,
+              enlargeStrategy: CenterPageEnlargeStrategy.height,
+            ),
+            items: Category.categories
+                .map((category) => HeroCarouselCard(category: category))
+                .toList(),
+          )),
           SectionTitle(title: 'RECOMMENDED'),
+          //Product Card
+          ProductCard(product: Product.products[0],)
         ],
       ),
     );
   }
 }
-
-
-
