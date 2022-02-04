c916fac98864190170a77dc844d8305425f41650
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-02-04 10:13:25 +0800

    implement BloC pattern in wishlish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_codelab/blocs/wishlist/wishlist_bloc.dart';
 import 'package:flutter_codelab/screens/screens.dart';
 
 
@@ -15,12 +17,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'E-Commerce',
-      theme: theme(),
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      initialRoute: HomeScreen.routeName,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => WishlistBloc()..add(StartWishlist()))
+      ],
+      child: MaterialApp(
+        title: 'E-Commerce',
+        theme: theme(),
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        initialRoute: HomeScreen.routeName,
 
+      ),
     );
   }
 }
